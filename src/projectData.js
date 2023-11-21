ce5432c18406e08e99075d24a17db995ed7076ad
Hrithik Khadka <hritik.khadka@centria.fi>
2023-11-21 19:22:17 +0200

    create project data file

---
+++ src/projectData.js
@@ -0,0 +1,36 @@
+const projects = [
+  {
+    id: 1,
+    title: "MiniHSL",
+    description:
+      "A react based web application where users can see and filter busses arriving at a station.",
+    image: "/images/mini-hsl.PNG",
+    link: "https://github.com/hrithikhadka/mini-HSL",
+  },
+  {
+    id: 2,
+    title: "Portfolio",
+    description:
+      "My personal portoflio built using react and tailwind css demonstrating my projects and skills.",
+    image: "/images/hrithikhadka.PNG",
+    link: "https://github.com/hrithikhadka/react-portfolio",
+  },
+  {
+    id: 3,
+    title: "NextDo",
+    description:
+      "A todo application made with NextJS and MongoDB. A user can create, update and delete a todo.",
+    image: "/images/nextjstodo.png",
+    link: "https://github.com/hrithikhadka/NextDo",
+  },
+  {
+    id: 4,
+    title: "Full Stack Open",
+    description:
+      "My submission for Full Stack Open exercises will be uploaded here. Check for updates on my progress.",
+    image: "/images/fullstackopen.PNG",
+    link: "https://github.com/hrithikhadka/FullStack-Open",
+  },
+];
+
+export default projects;
