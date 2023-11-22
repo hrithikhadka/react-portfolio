57237214483658fcebeb6d2ff68ff63e6ecf28a5
Hrithik Khadka <hritik.khadka@centria.fi>
2023-11-22 17:48:46 +0200

    feat: Use project data dynamically in SelectedProjects

--- src/components/SelectedProjects.jsx
+++ src/components/SelectedProjects.jsx
@@ -1,90 +1,47 @@
 import { BsArrowUpRight } from "react-icons/bs";
 import { BsArrowRight } from "react-icons/bs";
+import projects from "../projectData";
+
+const Project = ({ title, description, image, link }) => {
+  return (
+    <div className="flex flex-col h-full">
+      <div className="relative mx-auto max-w-md rounded-lg bg-gradient-to-tr from-pink-300 to-blue-300 p-0.5 shadow-lg">
+        <div className="bg-white p-7 rounded-md">
+          <img src={image} alt={title} className="w-96 h-48 object-cover" />
+
+          <h1 className="font-bold text-xl mb-2 mt-2">{title}</h1>
+          <p>{description}</p>
+          <div className="flex items-center gap-1 my-2.5 cursor-pointer">
+            <p>Preview</p>
+            <BsArrowUpRight />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const SelectedProjects = () => {
   return (
     <div>
       <h2 className="text-[24px] my-[20px]">Selected Work</h2>
       <figure>
-        <img src="/images/mernblog.PNG" />
+        <img src="/images/mernblog.PNG" alt="Project Preview" />
         <div className="flex items-center gap-1 mt-3">
           <h1 className="text-xl cursor-pointer">Blog Application</h1>
           <BsArrowUpRight className="cursor-pointer text-xl" />
         </div>
       </figure>
-      <div className="grid grid-cols-2 gap-2 mt-[3rem]">
-        <div className="relative mx-auto max-w-md rounded-lg bg-gradient-to-tr from-pink-300 to-blue-300 p-0.5 shadow-lg">
-          <div className="bg-white p-7 rounded-md">
-            <img src="/images/mini-hsl.PNG" />
-            <h1 className="font-bold text-xl mb-2 mt-2">Mini HSL</h1>
-            <p>
-              A react based web application where users can see and filter
-              busses arriving at a station.
-            </p>
-            <div className="flex items-center gap-1 my-3.5 cursor-pointer">
-              <p>Preview</p>
-              <BsArrowUpRight />
-            </div>
-          </div>
-        </div>
-        <div className="relative mx-auto max-w-md rounded-lg bg-gradient-to-tr from-pink-300 to-blue-300 p-0.5 shadow-lg">
-          <div className="bg-white p-7 rounded-md">
-            <img src="/images/hrithikhadka.PNG" />
 
-            <h1 className="font-bold text-xl mb-2">Portfolio</h1>
-            <p>
-              My personal portoflio built using react and tailwind css
-              demonstrating my projects and skills
-            </p>
-            <div className="flex items-center gap-1 my-2 cursor-pointer">
-              <p>Preview</p>
-              <BsArrowUpRight />
-            </div>
-          </div>
-        </div>
-      </div>{" "}
-      <div className="grid grid-cols-2 gap-2 mt-[3rem]">
-        <div className="relative mx-auto max-w-md rounded-lg bg-gradient-to-tr from-pink-300 to-blue-300 p-0.5 shadow-lg">
-          <div className="bg-white p-7 rounded-md">
-            <img src="/images/nextjstodo.png" />
+      <div className="grid grid-cols-2 gap-10 mt-[3rem]">
+        {projects.map((project, index) => (
+          <Project key={index} {...project} />
+        ))}
+      </div>
 
-            <h1 className="font-bold text-xl mb-2 mt-2">NextDo</h1>
-            <p>
-              A todo application built with NextJS and MongoDB. A user can
-              create, update and delete a todo.
-            </p>
-            <div className="flex items-center gap-1 my-7 cursor-pointer">
-              <p>Preview</p>
-              <BsArrowUpRight />
-            </div>
-          </div>
-        </div>
-        <div className="relative mx-auto max-w-md rounded-lg bg-gradient-to-tr from-pink-300 to-blue-300 p-0.5 shadow-lg">
-          <div className="bg-white p-7 rounded-md">
-            <img src="/images/fullstackopen.PNG" />
-
-            <h1 className="font-bold text-xl mb-2">Full Stack Open</h1>
-            <p>
-              I am learning full stack development from this course. My
-              submission for this course exercises will be uploaded here.
-            </p>
-            <div className="flex items-center gap-1 my-3 cursor-pointer">
-              <p>Preview</p>
-              <BsArrowUpRight />
-            </div>
-          </div>
-        </div>
-        <div className="flex items-center">
-          <a
-            href="https://github.com/hrithikhadka?tab=repositories"
-            target="_blank"
-            rel="noreferrer"
-            className="text-[24px] my-[20px] cursor-pointer"
-          >
-            View all Work
-          </a>
-          <BsArrowRight className="text-2xl mt-1 ml-2 cursor-pointer" />
-        </div>
+      <div className="flex items-center">
+        <h2 className="text-[24px] my-[20px]">View all Work</h2>
+        <BsArrowRight className="text-2xl mt-1 ml-2" />
       </div>
     </div>
   );
