91564136f75892b9d38a7d55759657d98af099e4
Hrithik Khadka <hritik.khadka@centria.fi>
2023-11-24 09:54:11 +0200

    implement responsive design for profile component

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,13 +2,13 @@ import { BsArrowRight } from "react-icons/bs";
 const Profile = () => {
   return (
     <div className="flex justify-between items-center">
-      <section className="mt-16">
-        <h1 className="text-5xl font-bold tracking-[-.15rem] leading-[1.15] mb-10">
+      <section className="sm:mt-16">
+        <h1 className="sm:text-[2rem] md:text-[3rem] sm:font-bold sm:tracking-[-.15rem] sm:leading-[1.15] mb-10">
           Hey 👋 I'm Hritik, a frontend developer based in Helsinki. I have
           experience across web and mobile development. I'm currently learning
           full stack development.
         </h1>
-        <p className="text-[1.3rem] leading-[1.6] flex items-center">
+        <p className="sm:text-[1.3rem] leading-[1.6] flex items-center">
           <a
             href="https://drive.google.com/drive/folders/1G-5E4R6ONqFpSE-nNXOQzxeTUAKpq51V"
             target="_blank"
@@ -16,7 +16,7 @@ const Profile = () => {
           >
             Resume
           </a>
-          <BsArrowRight className="text-2xl mt-1 ml-2" />
+          <BsArrowRight className="sm:text-2xl mt-1 ml-1.5" />
         </p>
       </section>
     </div>
