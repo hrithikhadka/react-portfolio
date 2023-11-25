02f0b8bd1e9ac5dd6e90c68d751afe25aaa009f7
Hrithik Khadka <hritik.khadka@centria.fi>
2023-11-25 19:03:14 +0200

    navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,20 +4,20 @@ const Navbar = () => {
   return (
     <>
       <nav className="font-normal flex justify-between mb-[24px]">
-        <div className="text-lg">
+        <div className="sm:text-lg">
           <a href="/">Hritik Khadka</a>
         </div>
         <div className="flex space-x-2">
-          <a href="/" className="text-lg">
+          <a href="/" className="sm:text-lg">
             About
           </a>
 
-          <a href="/" className="text-lg">
+          <a href="/" className="sm:text-lg">
             Work
           </a>
 
           <div className=" bg-[#e6e6e6] p-[6px] rounded-[4px]">
-            <MdOutlineLightMode className="text-2xl cursor-pointer" />
+            <MdOutlineLightMode className="sm:text-2xl cursor-pointer" />
           </div>
         </div>
       </nav>
