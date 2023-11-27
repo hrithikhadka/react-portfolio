3ce52c583f98c64d7c71f7ad9b2bf26454fcceda
Hrithik Khadka <hritik.khadka@centria.fi>
2023-11-27 19:33:58 +0200

    make contact component responsive

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,8 +2,10 @@ const Contact = () => {
   return (
     <div className="mb-16">
       <hr className="h-px my-8 bg-gray-400 border-0 dark:bg-gray-700"></hr>
-      <h3 className="text-[24px] my-[9px]">Get in touch</h3>
-      <p className="text-xl">
+      <h3 className="sm:text-[24px] max-sm:text-[19px] my-[9px]">
+        Get in touch
+      </h3>
+      <p className="sm:text-xl max-sm:text-[15px]">
         You can find me on{" "}
         <span className="text-blue-500 cursor-pointer">
           <a
